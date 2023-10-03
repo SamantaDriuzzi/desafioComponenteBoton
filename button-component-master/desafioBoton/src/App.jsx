3220f9969e1443b93256bf7b888c60c3da4f096e
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-10-03 14:18:53 -0300

    Instalo prop-types y styled-components,para seguir avanzando en la lógica del componente

--- button-component-master/desafioBoton/src/App.jsx
+++ button-component-master/desafioBoton/src/App.jsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <h1>Buttons</h1>
-      <Button />
+      <div className="container-buttons">
+        <Button>Default</Button>
+        <Button variant={"outline"}>Default</Button>
+        <Button variant={"text"}>Default</Button>
+        <Button disableShadow>Default</Button>
+        <Button disabled>Default</Button>
+        <Button variant={"text"} disabled>
+          Default
+        </Button>
+      </div>
     </>
   );
 }
--- button-component-master/desafioBoton/src/component/Button.jsx
+++ button-component-master/desafioBoton/src/component/Button.jsx
@@ -1,14 +1,92 @@
-import "../scss/component/Button.scss";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const Button = () => {
+const StyledButton = styled.button`
+  width: 81px;
+  height: 36px;
+  flex-shrink: 0;
+  border-radius: 6px;
+  text-align: center;
+  font-family: "Noto Sans JP";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  box-sizing: border-box;
+  padding: 10px;
+
+  background-color: ${(props) =>
+    props.variant === "outline"
+      ? "transparent"
+      : props.variant === "text"
+      ? "transparent"
+      : props.disableShadow
+      ? "#3d5afe"
+      : props.disabled
+      ? "#e0e0e0"
+      : props.variant === "text" && props.disabled
+      ? "transparent"
+      : "#e0e0e0"};
+
+  color: ${(props) =>
+    props.variant === "outline"
+      ? "#3d5afe"
+      : props.variant === "text" && !props.disabled
+      ? "#3d5afe"
+      : props.disableShadow
+      ? "#FFF"
+      : props.disabled
+      ? "#9E9E9E"
+      : props.disabled && props.variant === "text"
+      ? "#9E9E9E"
+      : "#3f3f3f"};
+
+  border: ${(props) =>
+    props.variant === "outline" ? "1px solid #3d5afe" : "none"};
+
+  box-shadow: ${(props) =>
+    props.variant === "outline"
+      ? "none"
+      : props.variant === "text"
+      ? "none"
+      : props.disableShadow
+      ? "none"
+      : "0px 2px 3px 0px rgba(51, 51, 51, 0.2)"};
+
+  &:hover,
+  &:focus {
+    background-color: ${(props) =>
+      props.variant === "outline"
+        ? "rgba(41, 98, 255, 0.10)"
+        : props.variant === "text" && !props.disabled
+        ? "rgba(41, 98, 255, 0.10)"
+        : props.variant === "text" && props.disabled
+        ? "transparent"
+        : props.disableShadow
+        ? "#3d5afe"
+        : props.disabled
+        ? "#e0e0e0"
+        : "#aeaeae"};
+  }
+`;
+
+const Button = ({ variant, disabled, disableShadow, children }) => {
+  Button.propTypes = {
+    variant: PropTypes.string,
+    disabled: PropTypes.bool,
+    disableShadow: PropTypes.bool,
+    children: PropTypes.node,
+  };
+
   return (
-    <div className="card">
-      <button>Default</button>
-      <button className="button-variant-outline">Default</button>
-      <button className="button-variant-text"> text</button>
-      <button className="button-disable-shadow"> disable shadow</button>
-      <button className="button-disabled"> disabled</button>
-    </div>
+    <StyledButton
+      variant={variant}
+      disabled={disabled}
+      disableShadow={disableShadow}
+    >
+      {children}
+    </StyledButton>
   );
 };
+
 export default Button;
