9607e3f8b937f6c4b78707c0ea0731063973d45b
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-10-19 19:13:35 -0300

    se crea la sidebar y se hacer ajustes en los botones

---
+++ button-component-master/desafioBoton/src/component/sidebar/Sidebar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "../../scss/component/Sidebar/Sidebar.scss";
+
+const Sidebar = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const menuItems = [
+    { id: "colors", label: "Colors" },
+    { id: "typography", label: "Typography" },
+    { id: "space", label: "Space" },
+    { id: "buttons", label: "Buttons" },
+    { id: "inputs", label: "Inputs" },
+    { id: "grids", label: "Grids" },
+  ];
+
+  const handleButtonClick = (id) => {
+    setActiveButton(id);
+  };
+
+  return (
+    <div>
+      <h2>
+        <span>Dev</span>challenges.io
+      </h2>
+      <ul>
+        {menuItems.map((item) => (
+          <li key={item.id}>
+            <a
+              href={`#${item.id}`}
+              className={activeButton === item.id ? "active" : ""}
+              onClick={() => handleButtonClick(item.id)}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
